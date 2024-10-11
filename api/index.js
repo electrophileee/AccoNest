a1685d16962b1d7687f81da9ac6c6b15db11e3fb
Abhishek <abhisheksahu1908@gmail.com>
2024-10-11 17:51:15 +0530

    remove unwanted code

--- api/index.js
+++ api/index.js
@@ -104,16 +104,7 @@ app.post('/api/logout', (req, res) => {
 });
 
 
-// app.post('/api/upload-by-link', async (req,res) => {
-//   const {link} = req.body;
-//   const newName = 'photo' + Date.now() + '.jpg';
-//   await imageDownloader.image({
-//     url: link,
-//     dest: '/tmp/' +newName,
-//   });
-//   const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
-//   res.json(url);
-// });
+
 app.post('/api/upload-by-link', async (req, res) => {
 
   try {
